7f0f09610e34c641fd1f582a7f40556e92cf29e4
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2015-11-10 21:14:00 -0800

    ajax-ify comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,12 @@ class CommentsController < ApplicationController
   def create
     permitted_params = params.require(:comment).permit(:content)
     @comment = Comment.new(permitted_params)
-    @comment.save
-    redirect_to root_path
+    
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
   end
 end
