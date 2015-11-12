cfceff06a88e7267c9a4fbec432eac629f94e0b6
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2015-11-11 18:47:27 -0800

    migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151108043326) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "content"
     t.datetime "created_at"
